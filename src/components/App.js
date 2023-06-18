731b72cfa8a214c0f349e73a078aff2507f22430
skshig <skshig@gmail.com>
2023-06-18 17:34:44 +0530

    submitting code-Sun Jun 18 2023 17:34:41 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, {Component, useState} from "react";
+import ReactDOM from "react-dom";
 import '../styles/App.css';
 
-const App = () => {
+function App() {
   return (
-    <div id="main">
-      <p>"I am learning React. My life is getting better."</p>
+    <div className="App">
+      <p>I am learning React. My life is getting better.</p>
     </div>
-  )
+  );
 }
-
 
 export default App;
