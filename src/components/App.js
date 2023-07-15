a5f5e30b6d022b7cb0bd696148fe0a7585709463
skshig <skshig@gmail.com>
2023-07-15 12:53:44 +0530

    infiniteth commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,11 @@ import '../styles/App.css';
 
 function App() {
   return (
+    
     <div id="main">
       <p>I am learning React. My life is getting better. </p>
     </div>
   );
 }
 
-export default App; 
+export default App;
